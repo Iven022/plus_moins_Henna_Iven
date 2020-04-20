fa8122f18aa580d7f613734db0f7bd8d5c5221aa
Iven022 <ivenmoonienxd@gmail.com>
2020-04-20 14:44:40 +0400

    commit pour la question no8

--- script/plus_moins.c
+++ script/plus_moins.c
@@ -32,18 +32,52 @@ int main()
  	
 	printf("Bienvenue au jeu de bingo\n");
 	printf("\n");
-
-	int choix_jouere,val_a_trouve;
+	int choix_jouere,val_a_trouve,mode;
 
 
 	srand(time(NULL));
 	val_a_trouve = rand() % 101;
-	printf("La valeur a trouver est : %d \n",val_a_trouve);
 	
-	
+
 	printf("\033[0m");
 	printf("\n");
 	
+	
+	do
+	{
+		printf("Choisissez la difficulté (1,2 or 3)\n");
+		printf("1) Facile : tentatives illimitée\n");
+		printf("2) Moyen : 25 tentatives\n");
+		printf("3) Difficile : 10 tentatives\n");
+		printf("Votre choix : ");
+		scanf("%d",&mode);
+
+		if ((mode>3) || (mode<1))
+		{
+			printf("Erreur, re-essayez\n");
+			printf("\n");
+		}
+
+	}while(mode>3 || mode<1);
+
+
+
+	switch(mode)
+	{
+		case 1:
+			mode=10000;
+			break;
+		case 2:
+			mode=25;
+			break;
+		case 3:
+			mode=10;
+			break;
+	}	
+
+	printf("\n");
+	
+	int x=0;//The variable x is used to count the number of tries of the user
 
 	do
 	{
@@ -51,6 +85,17 @@ int main()
 		scanf("%d",&choix_jouere);
 		plus_moins(choix_jouere,val_a_trouve);//Here the Function is called
 
-	}while(choix_jouere!=val_a_trouve);
-	
+		if (choix_jouere==val_a_trouve)
+		{
+			break;
+		}
+
+		x+=1;
+
+		if (x==mode)//This statement will run on the last try
+		{
+			printf("Malheureusement vos nombres d'essais sont arrives a terme!\n");
+		}
+
+	}while(x!=mode);
 }
